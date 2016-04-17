9a620e11d29154abc8d42a5c6aa4822090be5807
Eugene Marcotte <emarcotte@gmail.com>
2016-04-17 16:10:16 -0400

    test levels with &level=name

--- src/LD35.js
+++ src/LD35.js
@@ -36,10 +36,15 @@ LD35.prototype.onload = function() {
 }
 
 LD35.prototype.loaded = function() {
+	this.playState = new PlayScreen(this);
 	// TODO: me.state.set(me.state.MENU, new TitleScreen());
 	me.state.set(me.state.INTRO, new RadmarsScreen(this));
 	me.state.set(me.state.TITLE, new TitleScreen(this));
-	me.state.set(me.state.PLAY, new PlayScreen(this));
+	me.state.set(me.state.PLAY, this.playState);
+
+	if(this.options.level) {
+		this.playState.setNextLevel(this.options.level);
+	}
 
 	me.pool.register("mainPlayer", PlayerEntity);
 	me.pool.register("shooter", EnemyShooter, true);
