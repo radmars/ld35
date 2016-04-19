24bb9dca18f30d4c2cb29d4be4a3eb81d98562bf
Adhesion <adhesionmusic@gmail.com>
2016-04-18 21:12:47 -0400

    Fix beast mode song

--- src/GameResources.js
+++ src/GameResources.js
@@ -91,7 +91,7 @@ var GameResources = (function() {
 		_Audio('ld35-boss-big_mess'),
 		_Audio('ld35-boss-mess'),
 		_Audio('ld35-boss-skel'),
-		_Audio('ld35-main-beast'),
+		_Audio('ld35-main-big_mess'),
 		_Audio('ld35-main-mess'),
 		_Audio('ld35-main-skel'),
 		_Audio('bomber-shoot'),
