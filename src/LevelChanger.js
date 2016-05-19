93e9ad5f91612f735bf0eee6b001d6be4a291618
Trevor Williams <whiltx@gmail.com>
2016-05-19 16:06:02 -0400

    LevelChanger gets its own file.  Wowzers!

---
+++ src/LevelChanger.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var LevelChanger = me.LevelEntity.extend({
+	init: function(x, y, settings) {
+		settings.fade = "#000";
+		settings.duration = 500;
+		settings.onLoaded = this.levelChanged.bind(this);
+		this.level = settings.to;
+		this._super(me.LevelEntity, 'init', [x, y, settings]);
+		this.body.gravity = 0;
+		// Not sure what ACTION_OBJECT is meant for, so here it means "regions on maps that trigger behavior when the player runs over them without impeding movement."
+		this.body.collisionType = me.collision.types.ACTION_OBJECT;
+		this.body.setCollisionMask(me.collision.types.PLAYER_OBJECT);
+	},
+
+	levelChanged: function() {
+		me.state.current().player.hp = this.hp;
+		me.state.current().player.changeAnimation("idle");
+	},
+
+	update: function(dt) {
+		this.body.update(dt);
+		me.collision.check(this);
+	},
+
+	onCollision: function(response, other) {
+		if(other == me.state.current().player) {
+			this.hp = me.state.current().player.hp;
+			globalSettings.level = this.level;
+
+			if (globalSettings.level == "level6" && !me.state.current().hitForMusic) {
+				me.audio.stop("ld35-main-skel");
+				me.audio.stop("ld35-main-mess");
+				me.audio.stop("ld35-main-big_mess");
+				me.audio.play("ld35-boss-skel", true, null, me.state.current().player.getMode() == "skel" ? me.state.current().musicVolume : 0.0);
+				me.audio.play("ld35-boss-mess", true, null, me.state.current().player.getMode() == "mess" ? me.state.current().musicVolume : 0.0);
+				me.audio.play("ld35-boss-big_mess", true, null, me.state.current().player.getMode() == "big_mess" ? me.state.current().musicVolume : 0.0);
+				me.state.current().hitForMusic = true;
+			}
+
+			this.goTo();
+		}
+		return false;
+	},
+})
